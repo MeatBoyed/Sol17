b45f3b09db25c45bf8d24f0419a8d9c4aba48b34
Charles <47355793+MeatBoyed@users.noreply.github.com>
2020-12-30 22:32:13 +0200

    Description size and colour selection functional

--- client/src/components/SingleItemPageComponents/DescriptionViewer.tsx
+++ client/src/components/SingleItemPageComponents/DescriptionViewer.tsx
@@ -14,33 +14,43 @@ interface Props {
 
 export const DescriptionViewer: React.FC<Props> = ({ id, name, price, description, colours }) => {
 
+  const [colourOption, setColourOption] = useState<String>("black")
+  const [sizeOption, setSizeOption] = useState<String>("m")
+
+
   return (
     <div id="DescriptionViewer">
       <div className="titleContainer">
         <h1>{ name }</h1>
-        <p>{ price }</p>
+        <p>${ price }</p>
       </div>
       <p className="colourText">Colour - Red</p>
       <div className="colourPickerContainer">
-        <button className="colourButton">
-          <button className="colourButtonInner black"></button>
-        </button>
-        <button className="colourButton">
-          <button className="colourButtonInner white"></button>
-        </button>
-        <button className="colourButton">
-          <button className="colourButtonInner red"></button>
-        </button>
+        {colours.map((colour, index) => (
+          <button key={index} className="colourButton" style={{ backgroundColor: colourOption==colour ? 'lightblue' : 'white' }} >
+            <button className={`colourButtonInner ${colour}`} onClick={() => setColourOption(colour)}></button>
+          </button>
+        ))}
       </div>
       <div className="sizePicker">
-        <button className="sizeButton">XS</button>
-        <button className="sizeButton">S</button>
-        <button className="sizeButton" style={{ background: '#F84C4C' }}>
+        <button className="sizeButton" onClick={() => setSizeOption("xs")} style={{ background: sizeOption=="xs" ? '#F84C4C' : "white" }} >
+          XS
+        </button>
+        <button className="sizeButton" onClick={() => setSizeOption("s")} style={{ background: sizeOption=="s" ? '#F84C4C' : "white" }} >
+          S
+        </button>
+        <button className="sizeButton" onClick={() => setSizeOption("m")} style={{ background: sizeOption=="m" ? '#F84C4C' : "white" }} >
           M
         </button>
-        <button className="sizeButton">L</button>
-        <button className="sizeButton">XL</button>
-        <button className="sizeButton">XXL</button>
+        <button className="sizeButton" onClick={() => setSizeOption("l")} style={{ background: sizeOption=="l" ? '#F84C4C' : "white" }} >
+          L
+        </button>
+        <button className="sizeButton" onClick={() => setSizeOption("xl")} style={{ background: sizeOption=="xl" ? '#F84C4C' : "white" }} >
+          XL 
+        </button>
+        <button className="sizeButton" onClick={() => setSizeOption("xxl")} style={{ background: sizeOption=="xxl" ? '#F84C4C' : "white" }} >
+          XXL
+        </button>
       </div>
       <button className="AddToCartButton">Add To Cart</button>
       <div className="infoDropdown">
