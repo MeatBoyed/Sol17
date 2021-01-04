d93201a837f95d03ce600b8c4548a96f74952802
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-01-04 15:59:38 +0200

    Inital commit and removal of Firebase config data

--- client/src/firebaseConfig.tsx
+++ client/src/firebaseConfig.tsx
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBU3mZVcbyN2MDd0zgxE-bqPxwvg0C-qs",
-    authDomain: "sol-17.firebaseapp.com",
-    projectId: "sol-17",
-    storageBucket: "sol-17.appspot.com",
-    messagingSenderId: "793810952782",
-    appId: "1:793810952782:web:3cceb79434b8198fef191f",
-    measurementId: "G-PL04YWK255"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 firebase.initializeApp(firebaseConfig)
